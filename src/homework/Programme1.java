9be0188c64f22700c7d3cebd2f48f3b75085a291
HarryKing7276 <harryking7276@gmail.com>
2023-05-09 20:23:04 +0100

    Homework submitted

---
+++ src/homework/Programme1.java
@@ -0,0 +1,19 @@
+package homework;
+
+public class Programme1 {
+    public static void main (String[] args){
+        System.out.println("                        BIODATA                            ");
+        System.out.println("Name            : Harry Porter");
+        System.out.println("Mobile Number   : 0795263355");
+        System.out.println("Gender          : Male");
+        System.out.println("Date of Birth   : 01/01/2003");
+        System.out.println("Nationality     : British");
+        System.out.println("Religion        : Catholic");
+        System.out.println("Father`s name   : Jerry Porter");
+        System.out.println("Mother`s name   : Julie Porter");
+        System.out.println("Languages Known : English,Korean,Chinese,French,Hindi");
+        System.out.println("Hobbies         : Skating,Football,Photography,Dancing");
+        System.out.println("Address         : 7,Golden Street, California, OO6 3JN");
+    }
+}
+
---
+++ src/homework/Programme9.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class Programme9 {
+    public static void main(String[] args){
+        int a = -5; int b = 8; int c = 6;
+        System.out.println (a+b*c);
+        int d = 55; int e = 9; int k = 9;
+        System.out.println ((d+e)%k);
+        int x = 20; int y = -3; int z = 5; int v = 8;
+        System.out.println(x+y*z/v);
+        int f = 5; int g = 15;int h = 3; int i = 2;int j = 8;int m = 3;
+        System.out.println(f+g/h*i-j%m);
+    }
+}
